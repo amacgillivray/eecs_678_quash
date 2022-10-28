f370e9a561fd33a89f709eb9975cd1cbac22ba96
Andrew MacGillivray <agjmacgill@gmail.com>
2022-10-28 17:48:36 -0500

    WIP changes to job.rs Working on forwarding stdin/stdout

--- src/quash/job.rs
+++ src/quash/job.rs
@@ -1,4 +1,7 @@
+use std::os::unix::prelude::AsRawFd;
 use std::process;
+use std::io;
+use std::fs::File;
 use nix::sys::signal::{Signal, SigSet};
 use nix::unistd::Pid;
 
@@ -31,24 +34,42 @@ impl Job {
 
 impl Quash {
     pub fn run_job(&self, job: &Job) {
-        
+        use std::io::Write;
         // Init empty stdin
         // Init empty stdout
+        let stdin = io::stdin();
+        let stdout = io::stdout();
 
         for cmd in &job.cmds[..] {
 
             // if read:
                 // Override stdin input
                 // nix::unistd::dup2
+            if cmd.read != None {
+                nix::unistd::dup2(stdin.as_raw_fd(), stdout.as_raw_fd());
+            }
 
             // if write / append
                 // Override stdout with file
-            
+            if cmd.write != None {
+                let mut file = File::create(&cmd.args[0]).expect("Error: Unable to open file.");
+                let mut outputs = "";
+
+                // for line :
+                // file.write_all(stdout);
+            } else if cmd.append != None {
+                let mut file = File::create(&cmd.args[0]).expect("Error: Unable to open file.");
+                // for line :
+                // file.write_all(stdout);
+            }
+
+
             // Always override stdout
 
             self.exec_cmd(&cmd);
 
             // Stdout => stdin fof next
+            nix::unistd::dup2(stdout.as_raw_fd(), stdin.as_raw_fd());
         }
 
         // Println stdout
